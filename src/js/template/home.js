260b5d28af8fb952fcabb00da9b542b2536bfd0a
youliso <youliso@hotmail.com>
2019-10-13 16:47:33 +0800

    重构

---
+++ src/js/template/home.js
@@ -0,0 +1,11 @@
+'use strict';
+const Home = () => {
+    return `
+           <div>
+             ${new Date()}
+           </div>
+           `;
+};
+module.exports = {
+    Home
+};
--- src/js/view/index.js
+++ src/js/view/index.js
@@ -1,1 +1,5 @@
-const _ = require('../lib/util');
+'use strict';
+const Home_template = require('../template/home');
+const Home_event = require('../event/home');
+const _ = require('../apis/util');
+Home_event.content.innerHTML = Home_template.Home();
