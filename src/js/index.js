d0343f90b0dcf1550dac691a473f9ed3073b785d
youliso <youliso@hotmail.com>
2019-10-13 17:18:24 +0800

    重构

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,5 @@ const Home_template = require('./template/home');
 const Home_event = require('./event/home');
 const _ = require('./api/util');
 Home_event.content.innerHTML = Home_template.Home();
+
+_.GetHttp({url:'https://hlds.zixutech.cn/index/server/GetServerPass'}).then(res=>console.log(res)).catch(err=>console.log(err));
