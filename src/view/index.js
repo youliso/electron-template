536ffd4a37e226cb7e289485b5fd7d8bcc0010b3
youliso <youliso@hotmail.com>
2019-10-13 15:48:28 +0800

    重构

--- src/view/index.js
+++ src/view/index.js
@@ -1,1 +1,4 @@
-console.log('index');
+const _ = require('../lib/util');
+_.GetHttp({url:"https://hlds.zixutech.cn/index/server/GetServerPass"}).then(res => {
+    console.log(res)
+}).catch(err => console.log(err));
