93b5200866127e4895e8329166fb4eb80291eb57
youliso <youliso@hotmail.com>
2019-11-21 11:34:07 +0800

    修改结构

--- src/main/route.js
+++ src/main/route.js
@@ -1,8 +1,8 @@
 module.exports = {
     pages: (Vue) => {
         const v = {
-            "home": require('./pages/home').home,
-            "login": require('./pages/login').login
+            "home": require('./views/home').home,
+            "login": require('./views/login').login
         };
         for (let key in v) Vue.component(key, v[key]);
     }
