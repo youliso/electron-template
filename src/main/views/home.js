cedaee435fdd0300250e5207fd427ed063141d94
youliso <youliso@hotmail.com>
2020-01-08 17:50:53 +0800

    init
    Signed-off-by: youliso <youliso@hotmail.com>

--- src/main/views/home.js
+++ src/main/views/home.js
@@ -5,17 +5,26 @@ module.exports = {
         data() {
             return {
                 count: 0,
-                date: new Date()
+                date: new Date(),
+                test: ''
             }
         },
         template: `<div class="subclass">
         <h4>{{date}}</h4>
+        <img :src="test">
         </div>`,
         created() {
             //首次加载
             this.r = setInterval(() => {
                 this.date = new Date();
             }, 1000);
+            fetch('https://picsum.photos/200').then(e => e.blob()).then(blob => {
+                let reader = new FileReader();
+                reader.readAsDataURL(blob);
+                reader.onload = (e) => {
+                    this.test = e.target.result;
+                }
+            });
         },
         beforeDestroy() {
             //卸载
@@ -27,8 +36,6 @@ module.exports = {
         deactivated() {
             //开启缓存后 切换卸载
         },
-        methods: {
-
-        }
+        methods: {}
     }
 };
