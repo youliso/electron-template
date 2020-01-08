679684d7c25c964e25d4eb3ecb931abef78ea152
youliso <youliso@hotmail.com>
2020-01-08 16:04:47 +0800

    init
    Signed-off-by: youliso <youliso@hotmail.com>

--- src/main/views/home.js
+++ src/main/views/home.js
@@ -10,7 +10,6 @@ module.exports = {
         },
         template: `<div class="subclass">
         <h4>{{date}}</h4>
-        <button @click="tks" class="button">测试弹框</button>  
         </div>`,
         created() {
             //首次加载
@@ -29,29 +28,7 @@ module.exports = {
             //开启缓存后 切换卸载
         },
         methods: {
-            tks() {
-                const ipAPI = 'https://api.ipify.org?format=json';
 
-                Swal.queue([{
-                    title: 'Your public IP',
-                    confirmButtonText: 'Show my public IP',
-                    text:
-                        'Your public IP will be received ' +
-                        'via AJAX request',
-                    showLoaderOnConfirm: true,
-                    preConfirm: () => {
-                        return fetch(ipAPI)
-                            .then(response => response.json())
-                            .then(data => Swal.insertQueueStep(data.ip))
-                            .catch(() => {
-                                Swal.insertQueueStep({
-                                    icon: 'error',
-                                    title: 'Unable to get your public IP'
-                                })
-                            })
-                    }
-                }])
-            }
         }
     }
 };
