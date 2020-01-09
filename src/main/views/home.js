555c86ff544e882487f5277a58e6a6ee6c7c6f5c
youliso <youliso@hotmail.com>
2020-01-09 10:29:01 +0800

    init
    Signed-off-by: youliso <youliso@hotmail.com>

--- src/main/views/home.js
+++ src/main/views/home.js
@@ -4,34 +4,48 @@ module.exports = {
     main: {
         data() {
             return {
-                count: 0,
-                date: new Date(),
-                test: '',
-                test_show: false
+                listData: []
             }
         },
         template: `<div class="subclass">
-        <h4>{{date}}</h4>
-        <transition name="fade">
-        <img v-if="test_show" width="100%;" :src="test">
-        </transition>
+           <div>更新列表</div>
+           <ul><li v-for="(v,index) in listData">
+                   <div>{{v.name}}</div>
+                   <div  v-for="(i,indez) in v.list"><a :href="i">{{i}}下载</a></div>
+                   <button  @click="Downer(v)" class="button">全部下载</button>
+               </li>
+            </ul>
         </div>`,
         created() {
-            //首次加载
-            this.r = setInterval(() => {
-                this.date = new Date();
-            }, 1000);
-           setInterval(()=>{
-               fetch('https://picsum.photos/950/400').then(e => e.blob()).then(blob => {
-                   this.test_show = false;
-                   let reader = new FileReader();
-                   reader.readAsDataURL(blob);
-                   reader.onload = (e) => {
-                       this.test = e.target.result;
-                       this.test_show = true;
-                   }
-               });
-           },10000)
+            fetch('https://www.tw.playblackdesert.com/News/Notice/Detail?boardNo=1579&countryType=zh-TW')
+                .then(e => e.text())
+                .then(e => {
+                    try {
+                        let start = e.indexOf('<body>');
+                        let end = e.indexOf('</body>');
+                        let el = document.createElement('html');
+                        el.innerHTML = e.substring(start, end);
+                        let cont = el.getElementsByClassName('contents_area')[0];
+                        let data = [];
+                        for (let i of cont.getElementsByTagName('table')) {
+                            let item = {
+                                name: '',
+                                list: []
+                            };
+                            for (let t of i.previousSibling.previousSibling.getElementsByTagName('strong')) {
+                                item.name += t.innerText;
+                            }
+                            for (let s of i.getElementsByTagName('a')) {
+                                item.list.push(s.getAttribute('href'));
+                            }
+                            data.push(item);
+                        }
+                        console.log(data);
+                        this.listData = data;
+                    } catch (e) {
+                        console.log(e);
+                    }
+                })
         },
         beforeDestroy() {
             //卸载
@@ -43,6 +57,20 @@ module.exports = {
         deactivated() {
             //开启缓存后 切换卸载
         },
-        methods: {}
+        methods: {
+            Downer(v) {
+                for (let i of v.list) {
+                    this.download(i.substring(i.lastIndexOf('/')), i);
+                }
+            },
+            download(name, href) {
+                let a = document.createElement("a"), //创建a标签
+                    e = document.createEvent("MouseEvents"); //创建鼠标事件对象
+                e.initEvent("click", false, false); //初始化事件对象
+                a.href = href; //设置下载地址
+                a.download = name; //设置下载文件名
+                a.dispatchEvent(e); //给指定的元素，执行事件click事件
+            }
+        }
     }
 };
