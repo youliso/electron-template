1d1062d010e7651ad84d082499d30ba9eb241cf4
youliso <youliso@hotmail.com>
2020-09-27 15:48:18 +0800

    更改渲染层为svelte

--- src/views/app.svelte
+++ src/views/app.svelte
@@ -4,28 +4,14 @@
 </script>
 
 <main>
-    <h1>Hello {remote.app.name}!!</h1>
+    <div>Hello {remote.app.name}!!</div>
 </main>
 
 <style>
-    main {
-        background: #333333;
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-
-    h1 {
+    div {
         color: #ff3e00;
         text-transform: uppercase;
         font-size: 4em;
         font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
     }
 </style>
