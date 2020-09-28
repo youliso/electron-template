35a78b627101c32f61410c8e8f3f822ddb8dcb7a
youliso <youliso@hotmail.com>
2020-09-28 14:47:53 +0800

    更改渲染层为svelte

--- src/views/app.svelte
+++ src/views/app.svelte
@@ -4,11 +4,22 @@
 </script>
 
 <main>
-    <div>Hello {remote.app.name}!!</div>
+    <div class="head">
+        <i on:click="{()=>ipcRenderer.send('closed')}" class="cuIcon-close no-drag cursor-pointer"></i>
+    </div>
+    <div class="title">Hello {remote.app.name}!!</div>
 </main>
 
 <style>
-    div {
+    .head{
+        height: 40px;
+        padding: 0 10px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        font-size: 20px;
+    }
+    .title {
         color: #ff3e00;
         text-transform: uppercase;
         font-size: 4em;
