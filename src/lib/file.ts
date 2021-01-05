607ef04e8dce1d567a1c6c9e63a585ec5e7f5158
youliso <youliso@hotmail.com>
2021-01-05 19:18:12 +0800

    相对绝对路径问题

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -10,7 +10,7 @@ import {isNull} from "@/lib";
  * @param fileName
  */
 export function findFileBySuffix(path: string, fileName: string) {
-    if (path.substr(0, 1) !== "/") path = resolve(path);
+    if (path.substr(0, 1) !== "/" && path.indexOf(":") === -1) path = resolve(path);
     let files: string[] = [];
     let dirArray = fs.readdirSync(path)
     for (let d of dirArray) {
@@ -30,7 +30,7 @@ export function findFileBySuffix(path: string, fileName: string) {
  * 删除目录和内部文件
  * */
 export function delDir(path: string): void {
-    if (path.substr(0, 1) !== "/") path = resolve(path);
+    if (path.substr(0, 1) !== "/" && path.indexOf(":") === -1) path = resolve(path);
     let files = [];
     if (fs.existsSync(path)) {
         files = fs.readdirSync(path);
@@ -51,7 +51,7 @@ export function delDir(path: string): void {
  * @return 0 不存在 1 只可读 2 存在可读写
  */
 export function access(path: string) {
-    if (path.substr(0, 1) !== "/") path = resolve(path);
+    if (path.substr(0, 1) !== "/" && path.indexOf(":") === -1) path = resolve(path);
     return new Promise((resolve) =>
         fs.access(path, fs.constants.F_OK | fs.constants.W_OK, (err) => {
             if (err) err.code === 'ENOENT' ? resolve(0) : resolve(1);
@@ -66,7 +66,7 @@ export function access(path: string) {
  * @param options 选项
  */
 export function readFile(path: string, options?: { encoding?: BufferEncoding; flag?: string; }) {
-    if (path.substr(0, 1) !== "/") path = resolve(path);
+    if (path.substr(0, 1) !== "/" && path.indexOf(":") === -1) path = resolve(path);
     return new Promise((resolve) =>
         fs.readFile(
             path,
@@ -84,7 +84,7 @@ export function readFile(path: string, options?: { encoding?: BufferEncoding; fl
  * @param index
  */
 export function readLine(path: string, index?: number): Promise<string | any[]> | null {
-    if (path.substr(0, 1) !== "/") path = resolve(path);
+    if (path.substr(0, 1) !== "/" && path.indexOf(":") === -1) path = resolve(path);
     const io = createInterface({
         input: fs.createReadStream(path)
     });
@@ -121,7 +121,7 @@ export function readLine(path: string, index?: number): Promise<string | any[]> 
  * @return 0 失败 1 成功
  */
 export async function writeFile(path: string, data: string | Buffer, options?: { encoding?: BufferEncoding; mode?: number | string; flag?: string; }) {
-    if (path.substr(0, 1) !== "/") path = resolve(path);
+    if (path.substr(0, 1) !== "/" && path.indexOf(":") === -1) path = resolve(path);
     if (await access(path) === 0) fs.mkdirSync(dirname(path), {recursive: true});
     return new Promise((resolve) =>
         fs.writeFile(path, data, options, (err) => {
@@ -139,7 +139,7 @@ export async function writeFile(path: string, data: string | Buffer, options?: {
  * @return 0 失败 1 成功
  */
 export async function appendFile(path: string, data: string | Uint8Array, options?: { encoding?: BufferEncoding; mode?: number | string; flag?: string; }) {
-    if (path.substr(0, 1) !== "/") path = resolve(path);
+    if (path.substr(0, 1) !== "/" && path.indexOf(":") === -1) path = resolve(path);
     if (await access(path) === 0) fs.mkdirSync(dirname(path), {recursive: true});
     return new Promise((resolve) =>
         fs.appendFile(path, data, options, (err) => {
