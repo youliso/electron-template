884eb9f6d46b4ed4aa21b1c2c0fdf628997c720f
youliso <youliso@hotmail.com>
2021-02-26 11:05:17 +0800

    add .prettierrc

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,19 +1,19 @@
-import {resolve} from "path";
-import {app, globalShortcut, ipcMain} from "electron";
-import {IPC_MSG_TYPE} from "@/lib/interface";
-import Log from "@/lib/log";
-import {getExternPath} from "@/lib";
-import {readFile} from "@/lib/file";
-import {Session} from "./modular/session";
-import {Window} from "./modular/window";
-import {Update} from "./modular/update";
-import {Socket} from "./modular/socket";
-import {Platform} from "./platform";
+import { resolve } from 'path';
+import { app, globalShortcut, ipcMain } from 'electron';
+import { IPC_MSG_TYPE } from '@/lib/interface';
+import Log from '@/lib/log';
+import { getExternPath } from '@/lib';
+import { readFile } from '@/lib/file';
+import { Session } from './modular/session';
+import { Window } from './modular/window';
+import { Update } from './modular/update';
+import { Socket } from './modular/socket';
+import { Platform } from './platform';
 
 declare global {
     namespace NodeJS {
         interface Global {
-            sharedObject: { [key: string]: any }
+            sharedObject: { [key: string]: any };
         }
     }
 }
@@ -24,21 +24,20 @@ declare global {
 global.sharedObject = {
     isPackaged: app.isPackaged, //是否打包
     platform: process.platform, //当前运行平台
-    appInfo: { //应用信息
+    appInfo: {
+        //应用信息
         name: app.name,
         version: app.getVersion()
     }
 };
 
 class Init {
-
-    private window = new Window()
+    private window = new Window();
     private socket = new Socket();
     private update = new Update();
     private session = new Session();
 
-    constructor() {
-    }
+    constructor() {}
 
     /**
      * 初始化并加载
@@ -47,54 +46,53 @@ class Init {
         //协议调起
         let args = [];
         if (!app.isPackaged) args.push(resolve(process.argv[1]));
-        args.push("--");
+        args.push('--');
         app.setAsDefaultProtocolClient(app.name, process.execPath, args);
         app.allowRendererProcessReuse = true;
 
         if (!app.requestSingleInstanceLock()) {
             app.quit();
         } else {
-            app.on("second-instance", () => {
+            app.on('second-instance', () => {
                 // 当运行第二个实例时,将会聚焦到main窗口
                 if (this.window.main) {
                     if (this.window.main.isMinimized()) this.window.main.restore();
                     this.window.main.focus();
                 }
-            })
+            });
         }
 
-        app.on("window-all-closed", () => {
-            if (process.platform !== "darwin") {
+        app.on('window-all-closed', () => {
+            if (process.platform !== 'darwin') {
                 app.quit();
             }
-        })
+        });
 
-        app.on("activate", () => {
+        app.on('activate', () => {
             if (this.window.getAllWindows().length === 0) {
-                this.window.createWindow({isMainWin: true});
+                this.window.createWindow({ isMainWin: true });
             }
-        })
+        });
 
         //获得焦点时发出
-        app.on("browser-window-focus", () => {
+        app.on('browser-window-focus', () => {
             //关闭刷新
-            globalShortcut.register("CommandOrControl+R", () => {
-            });
+            globalShortcut.register('CommandOrControl+R', () => {});
         });
         //失去焦点时发出
-        app.on("browser-window-blur", () => {
+        app.on('browser-window-blur', () => {
             // 注销关闭刷新
-            globalShortcut.unregister("CommandOrControl+R");
+            globalShortcut.unregister('CommandOrControl+R');
         });
         /**
          * app重启
          */
-        ipcMain.on("app-relaunch", () => {
-            app.relaunch({args: process.argv.slice(1)});
+        ipcMain.on('app-relaunch', () => {
+            app.relaunch({ args: process.argv.slice(1) });
         });
         //app常用获取路径
-        ipcMain.on("app-path-get", (event, args) => {
-            event.returnValue = app.getPath(args.key)
+        ipcMain.on('app-path-get', (event, args) => {
+            event.returnValue = app.getPath(args.key);
         });
 
         //启动
@@ -102,7 +100,7 @@ class Init {
 
         //模块、创建窗口、托盘
         this.modular();
-        this.window.createWindow({isMainWin: true});
+        this.window.createWindow({ isMainWin: true });
         this.window.createTray();
     }
 
@@ -111,11 +109,11 @@ class Init {
      */
     async global() {
         try {
-            const setting = await readFile(getExternPath("setting.json"));
-            global.sharedObject["setting"] = JSON.parse(setting as string);
+            const setting = await readFile(getExternPath('setting.json'));
+            global.sharedObject['setting'] = JSON.parse(setting as string);
         } catch (e) {
-            Log.error("[setting]", e);
-            global.sharedObject["setting"] = {};
+            Log.error('[setting]', e);
+            global.sharedObject['setting'] = {};
         }
         Platform[global.sharedObject.platform]();
 
@@ -123,11 +121,11 @@ class Init {
          * 全局变量赋值
          * 返回1代表完成
          */
-        ipcMain.on("global-sharedObject-set", (event, args) => {
+        ipcMain.on('global-sharedObject-set', (event, args) => {
             global.sharedObject[args.key] = args.value;
             event.returnValue = 1;
         });
-        ipcMain.on("global-sharedObject-get", (event, args) => {
+        ipcMain.on('global-sharedObject-get', (event, args) => {
             event.returnValue = global.sharedObject[args.key];
         });
     }
@@ -139,13 +137,13 @@ class Init {
         /**
          * 消息反馈(根据需要增加修改)
          */
-        ipcMain.on("message-send", (event, args) => {
+        ipcMain.on('message-send', (event, args) => {
             switch (args.type) {
                 case IPC_MSG_TYPE.WIN: //window模块
-                    for (let i in this.window.group) if (this.window.group[i]) this.window.getWindow(Number(i)).webContents.send("message-back", args);
+                    for (let i in this.window.group) if (this.window.group[i]) this.window.getWindow(Number(i)).webContents.send('message-back', args);
                     break;
                 case IPC_MSG_TYPE.SOCKET: //socket模块
-                    if (this.socket.io && this.socket.io.io._readyState === "open") this.socket.io.send(args);
+                    if (this.socket.io && this.socket.io.io._readyState === 'open') this.socket.io.send(args);
                     break;
             }
         });
@@ -158,7 +156,6 @@ class Init {
         this.socket.on(this.window);
         this.update.on(this.window);
     }
-
 }
 
 /**
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <keep-alive :include="include" :exclude="exclude" :max="max">
-      <component :is="Component"/>
-    </keep-alive>
-  </router-view>
+    <router-view v-slot="{ Component }">
+        <keep-alive :include="include" :exclude="exclude" :max="max">
+            <component :is="Component" />
+        </keep-alive>
+    </router-view>
 </template>
 <script lang="ts">
-import {toRefs} from "vue";
-import {useRouter} from "vue-router";
-import {argsData, keepAliveOpt} from "./store";
+import { toRefs } from 'vue';
+import { useRouter } from 'vue-router';
+import { argsData, keepAliveOpt } from './store';
 
 export default {
-  setup() {
-    if (argsData.window.route) useRouter().replace(argsData.window.route);
-    return {...toRefs(keepAliveOpt)};
-  }
-}
+    setup() {
+        if (argsData.window.route) useRouter().replace(argsData.window.route);
+        return { ...toRefs(keepAliveOpt) };
+    }
+};
 </script>
 <style lang="scss">
-@import "views/scss/color";
-@import "views/scss/main";
+@import 'views/scss/color';
+@import 'views/scss/main';
 </style>
