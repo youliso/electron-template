c1e4d0eb0b86d276e569adb42722c3760fb2cfa4
youliso <youliso@hotmail.com>
2021-04-13 17:53:21 +0800

    删除多余配置

--- build/script/webpack.base.config.js
+++ build/script/webpack.base.config.js
@@ -25,9 +25,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif|ico|woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
+        type: 'asset/resource'
       }
     ]
   }
