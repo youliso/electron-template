a39d1b87f868ccdc6f4aa2098258011b98c3ede0
youliso <youliso@hotmail.com>
2021-06-22 15:33:43 +0800

    update session.ts (add cookies)

--- src/main/modular/session.ts
+++ src/main/modular/session.ts
@@ -1,61 +1,90 @@
-import {ipcMain, session} from "electron";
+import { ipcMain, session, CookiesGetFilter, CookiesSetDetails } from 'electron';
 
 /**
  * 监听
  */
 export class Session {
 
-    /**
-     * urls列表
-     */
-    public urls: string[] = [];
+  /**
+   * 头部 headers
+   * 键值对 => 域名: Headers
+   */
+  public urlHeaders: { [key: string]: { [key: string]: string } } = {};
 
-    /**
-     * 头部 headers
-     */
-    public urlHeaders: { [key: string]: { [key: string]: string } } = {}
+  constructor() {
+  }
 
-    constructor() {
-    }
+  /**
+   * 拦截指定http/https请求并更换、增加headers
+   */
+  webRequest() {
+    session.defaultSession.webRequest.onBeforeSendHeaders({
+      urls: [
+        'http://*/*',
+        'https://*/*'
+      ]
+    }, (details, callback) => {
+      const urls = Object.keys(this.urlHeaders);
+      const keys = urls.filter((key: string) => details.url.indexOf(key) === 0);
+      for (let key of keys) {
+        for (let v in this.urlHeaders[key]) {
+          details.requestHeaders[v] = this.urlHeaders[key][v];
+        }
+      }
+      callback({ requestHeaders: details.requestHeaders });
+    });
+  }
 
-    /**
-     * 拦截监听指定url请求并更换指定headers
-     */
-    webRequest() {
-        session.defaultSession.webRequest.onBeforeSendHeaders({
-            urls: this.urls
-        }, (details, callback) => {
-            const urls = Object.keys(this.urlHeaders);
-            const keys = urls.filter((key: string) => {
-                return details.url.indexOf(key) === 0;
-            })
-            for (let key of keys) {
-                for (let v in this.urlHeaders[key]) {
-                    details.requestHeaders[v] = this.urlHeaders[key][v];
-                }
-            }
-            callback({requestHeaders: details.requestHeaders});
-        });
-    }
+  /**
+   * 设置setUserAgent/acceptLanguages
+   * @param userAgent
+   * @param acceptLanguages
+   */
+  setUserAgent(userAgent: string, acceptLanguages?: string) {
+    session.defaultSession.setUserAgent(userAgent, acceptLanguages);
+  }
 
-    /**
-     * 设置setUserAgent/acceptLanguages
-     * @param userAgent
-     * @param acceptLanguages
-     */
-    setUserAgent(userAgent: string, acceptLanguages?: string) {
-        session.defaultSession.setUserAgent(userAgent, acceptLanguages);
-    }
+  /**
+   * 获取 Cookies
+   * @param filter
+   */
+  getCookies(filter: CookiesGetFilter) {
+    return session.defaultSession.cookies.get(filter);
+  }
 
-    /**
-     * 开启监听
-     */
-    on() {
-        this.webRequest();
-        //设置headers
-        ipcMain.on("session-set-headers", async (event, args) => {
-            this.urlHeaders = Object.assign(this.urlHeaders, args);
-        });
-    }
+  /**
+   * 设置 Cookies
+   * 如果存在，则会覆盖原先 cookie.
+   * @param details
+   */
+  setCookies(details: CookiesSetDetails) {
+    return session.defaultSession.cookies.set(details);
+  }
+
+  /**
+   * 移除 Cookies
+   * @param url
+   * @param name
+   */
+  removeCookies(url: string, name: string) {
+    return session.defaultSession.cookies.remove(url, name);
+  }
+
+  /**
+   * 开启监听
+   */
+  on() {
+    this.webRequest();
+    //设置url请求头
+    ipcMain.on('session-headers-set', async (event, args) => {
+      this.urlHeaders = Object.assign(this.urlHeaders, args);
+    });
+    //设置 Cookies
+    ipcMain.handle('session-cookies-set', async (event, args) => this.setCookies(args));
+    //获取 Cookies
+    ipcMain.handle('session-cookies-get', async (event, args) => this.getCookies(args));
+    //移除 Cookies
+    ipcMain.handle('session-cookies-remove', async (event, args) => this.removeCookies(args.url, args.name));
+  }
 
 }
--- src/renderer/utils/session.ts
+++ src/renderer/utils/session.ts
@@ -1,6 +1,34 @@
+import { CookiesGetFilter, CookiesSetDetails } from 'electron';
+
 /**
- * 指定headers
+ * 设置http/https指定域名请求头
+ * 键值对 => 域名: Headers
  */
-export function sessionSetHeaders(args: { [key: string]: { [key: string]: string } }) {
-    window.ipcFun.send("session-set-headers", args);
+export function sessionHeadersSet(args: { [key: string]: { [key: string]: string } }) {
+  window.ipcFun.send('session-headers-set', args);
+}
+
+/**
+ * 获取 cookies
+ * @param args
+ */
+export function sessionCookiesGet(args: CookiesGetFilter) {
+  return window.ipcFun.invoke('session-cookies-get', args);
+}
+
+/**
+ * 设置 cookies
+ * @param args
+ */
+export function sessionCookiesSet(args: CookiesSetDetails) {
+  return window.ipcFun.invoke('session-cookies-set', args);
+}
+
+/**
+ * 移除 Cookies
+ * @param url
+ * @param name
+ */
+export function sessionCookiesRemove(url: string, name: string) {
+  return window.ipcFun.invoke('session-cookies-remove', { url, name });
 }
