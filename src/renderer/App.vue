19f249d777665b31ff96d4ddf12914de6ac5c977
youliso <youliso@Hotmail.com>
2021-07-13 16:08:22 +0800

    update App.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <keep-alive
-      :include="keepAliveData.include"
-      :exclude="keepAliveData.exclude"
-      :max="keepAliveData.max"
-    >
-      <component :is="Component" />
-    </keep-alive>
+  <router-view v-slot="{ Component, route }">
+    <transition :name="route.meta.transition || 'fade'" mode="out-in">
+      <keep-alive
+        :include="keepAliveData.include"
+        :exclude="keepAliveData.exclude"
+        :max="keepAliveData.max"
+        :key="route.meta.usePathKey ? route.path : undefined"
+      >
+        <component :is="Component" />
+      </keep-alive>
+    </transition>
   </router-view>
 </template>
 <script lang="ts">
