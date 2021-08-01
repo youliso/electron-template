4edfc8dc7944156be2be8964b632a5fe232ab5cb
youliso <youliso@Hotmail.com>
2021-08-01 12:50:29 +0800

    update

--- src/main/platform.ts
+++ src/main/platform.ts
@@ -1,10 +1,14 @@
 export function win32() {
+  console.log('win32 platform');
 }
 
 export function linux() {
+  console.log('linux platform');
 }
 
-export function darwin() {}
+export function darwin() {
+  console.log('darwin platform');
+}
 
 export const Platforms: { [key: string]: Function } = {
   win32,
