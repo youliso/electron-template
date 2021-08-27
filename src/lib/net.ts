4bbcee0c5e815595dcbb81c50b8caa6e2ec189be
youliso <youliso@Hotmail.com>
2021-08-27 13:25:17 +0800

    fix bug

--- src/lib/net.ts
+++ src/lib/net.ts
@@ -98,7 +98,7 @@ function fetchPromise<T>(url: string, sendData: NetOpt): Promise<T> {
  * @param param
  */
 export async function net<T>(url: string, param: NetOpt = {}): Promise<T> {
-  if (url.indexOf('http://') === -1 && url.indexOf('https://') === -1) url = appUrl + url;
+  if (!url.startsWith('http://') && !url.startsWith('https://')) url = appUrl + url;
   let abort: TimeOutAbort = null;
   if (isNull(param.signal)) abort = timeOutAbort(param.timeout || timeout);
   let sendData: NetOpt = {
