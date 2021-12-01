25c4147535fc7620629843bd554185e3f7422cff
mlmdflr <liao520ly@gmail.com>
2021-12-01 14:44:55 +0800

    fix:getResourcesPath

--- src/main/modular/global.ts
+++ src/main/modular/global.ts
@@ -156,21 +156,15 @@ export class Global {
    * 获取资源文件路径
    * 不传path返回此根目录
    * */
-  getResourcesPath(type: 'platform' | 'inside' | 'extern' | 'root', path?: string): string {
+   getResourcesPath(type: 'platform' | 'inside' | 'extern' | 'root', path: string = './'): string {
     try {
       switch (type) {
         case 'platform':
-          if (!path) return app.isPackaged
-            ? resolve(join(__dirname, '..', '..', '..', 'platform', process.platform))
-            : resolve(join('resources', 'platform', process.platform))
-          path = app.isPackaged
+          path = normalize(app.isPackaged
             ? resolve(join(__dirname, '..', '..', '..', 'platform', process.platform, path))
-            : resolve(join('resources', 'platform', process.platform, path));
+            : resolve(join('resources', 'platform', process.platform, path)));
           break;
         case 'inside':
-          if (!path) return app.isPackaged
-            ? resolve(join(__dirname, '..', '..', 'inside'))
-            : resolve(join('resources', 'inside'))
           path = normalize(
             app.isPackaged
               ? resolve(join(__dirname, '..', '..', 'inside', path))
@@ -178,19 +172,13 @@ export class Global {
           );
           break;
         case 'extern':
-          if (!path) return app.isPackaged
-            ? resolve(join(__dirname, '..', '..', '..', 'extern'))
-            : resolve(join('resources', 'extern'))
           path = normalize(
             app.isPackaged
               ? resolve(join(__dirname, '..', '..', '..', 'extern', path))
               : resolve(join('resources', 'extern', path))
           );
           break;
-        case 'root':
-          if (!path) return app.isPackaged
-            ? resolve(join(__dirname, '..', '..', '..', '..'))
-            : resolve(join('resources', 'root'))
+        case 'root':  
           path = normalize(
             app.isPackaged
               ? resolve(join(__dirname, '..', '..', '..', '..', path))
@@ -198,7 +186,6 @@ export class Global {
           );
           break;
       }
-      path = normalize(path);
       accessSync(path, constants.R_OK);
       return path;
     } catch (e) {
