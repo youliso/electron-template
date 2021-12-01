63a1440171e8268f6dec3216f9500db266d107ab
youliso <youliso@hotmail.com>
2021-12-01 13:48:21 +0800

    fix: build

--- build/script/build.js
+++ build/script/build.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
-const readline = require('readline')
+const readline = require('readline');
 const webpack = require('webpack');
 const config = require('../cfg/build.json');
 const main = require('./webpack.main.config'); //主进程
 const renderer = require('./webpack.renderer.config'); //子进程
 let [, , arch] = process.argv;
 
-const optional = ['win', 'win32', 'win64', 'winp', 'winp32', 'winp64', 'darwin', 'mac', 'linux']
+const optional = ['win', 'win32', 'win64', 'winp', 'winp32', 'winp64', 'darwin', 'mac', 'linux'];
 
 const r = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-})
+});
 
 function deleteFolderRecursive(url) {
   let files = [];
@@ -43,10 +43,10 @@ config.extraResources = [
 function checkInput(str) {
   if (optional.indexOf(str) === -1) {
     console.log('illegal input');
-    r.close()
-    return false
+    r.close();
+    return false;
   }
-  return true
+  return true;
 }
 
 function core(arch) {
@@ -100,17 +100,15 @@ function core(arch) {
       to: archPath,
       filter: ['**/*']
     });
-  } catch (err) {
-    console.info(`[warn archPath ${archPath}]`, err);
-  }
-  fs.writeFileSync('./build/cfg/build.json', JSON.stringify(config, null, 2));//写入配置
+  } catch (err) {}
+  fs.writeFileSync('./build/cfg/build.json', JSON.stringify(config, null, 2)); //写入配置
   deleteFolderRecursive(path.resolve('dist')); //清除dist
   webpack([{ ...main('production') }, { ...renderer('production') }], (err, stats) => {
     if (err || stats.hasErrors()) throw err;
     console.log(`electron-builder -c build/cfg/build.json --${archTag}`);
     const s = setInterval(() => {
       console.log('building...');
-    }, 5000)
+    }, 5000);
     const c = exec(
       `electron-builder -c build/cfg/build.json --${archTag}`,
       {
@@ -126,9 +124,9 @@ function core(arch) {
       }
     );
     c.once('close', () => {
-      clearInterval(s)
-      r.close()
-    })
+      clearInterval(s);
+      r.close();
+    });
   });
 }
 
@@ -136,16 +134,16 @@ if (!arch) {
   console.log('Which platform is you want to build?');
   console.log(`optional：${optional}    q exit`);
   r.on('line', (str) => {
-    if (str === "q") {
+    if (str === 'q') {
       console.log('exit success');
-      r.close()
-      return
+      r.close();
+      return;
     }
-     if(!checkInput(str)) return
-    r.pause()
-    core(str)
-  })
+    if (!checkInput(str)) return;
+    r.pause();
+    core(str);
+  });
 } else {
-  if(!checkInput(arch)) return
-  core(arch)
+  if (!checkInput(arch)) return;
+  core(arch);
 }
