3141f72bdf0f269de8141158ba0020f0d11cf842
youliso <youliso@hotmail.com>
2022-02-02 23:40:16 +0800

    feat: update load

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -4,14 +4,11 @@ import GlobalComponent from '@/renderer/common/globalComponent';
 import '@/renderer/views/scss/color.scss';
 import '@/renderer/views/scss/index.scss';
 
-windowLoad(async (_, args) => {
+windowLoad((_, args) => {
   setCustomize(args);
-  if (args.title) document.title = args.title;
+  document.title = args.title || '';
   document.body.setAttribute('platform', window.environment.platform);
   document.body.setAttribute('headNative', args.headNative + '' || 'false');
-  !args.headNative &&
-    GlobalComponent.use(import('./views/components/head'), 'head').then(() =>
-      import('@/renderer/router').then(({ routerInit }) => routerInit(args.route as string))
-    );
+  !args.headNative && GlobalComponent.use(import('./views/components/head'), 'head');
   import('@/renderer/router').then(({ routerInit }) => routerInit(args.route as string));
 });
