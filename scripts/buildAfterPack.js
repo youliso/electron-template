28631db3ed41f7808914a2d9a234ae5df1c5d823
mlmdflr <liao520ly@gmail.com>
2022-03-07 10:24:19 +0800

    build log color

--- scripts/buildAfterPack.js
+++ scripts/buildAfterPack.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 exports.default = async ({ appOutDir, packager }) => {
   try {
     const asarPath = join(packager.getResourcesDir(appOutDir), 'app.asar');
-    console.log(`applying asarmor patches to ${asarPath}`);
+    console.log(`  \x1B[34m•\x1B[0m afterPack applying asarmor patches  \x1B[34mfile\x1B[0m=${asarPath}`);
     const archive = await asarmor.open(asarPath);
     archive.patch(); // apply default patches
     await archive.write(asarPath);
