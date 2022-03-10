0d31a8a469ff7e518cd59bce27684413285eada7
youliso <youliso@hotmail.com>
2022-03-10 22:01:20 +0800

    feat: update

--- src/renderer/views/pages/home/style/index.ts
+++ src/renderer/views/pages/home/style/index.ts
@@ -7,9 +7,4 @@ export default css`
   width: 100%;
   height: 100%;
   padding: ${args.headNative ? '10px' : '32px'} 10px 10px;
-  > .btn {
-    width: 100%;
-    color: var(--blue);
-    text-decoration: underline;
-  }
 `;
