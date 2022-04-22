f561823fc3fc8b729d5f71fab5a683fdad6b7ced
youliso <youliso@hotmail.com>
2022-04-22 21:23:13 +0800

    fix: bug

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,20 +1,23 @@
-import { appInstance, windowInstance, Tray, Session, Update } from 'ym-electron/main';
+import { appInstance, windowInstance, Tray, Session, Update, logError } from 'ym-electron/main';
 import { customize, opt } from '@/cfg/window.json';
 import updateCfg from '@/cfg/update.json';
 import logo from '@/assets/icon/logo.png';
 
-appInstance.start().then(async () => {
-  const tary = new Tray();
-  const update = new Update(
-    { provider: updateCfg.provider as any, url: updateCfg.url },
-    updateCfg.dirname
-  );
-  const session = new Session();
+appInstance
+  .start()
+  .then(() => {
+    const tary = new Tray();
+    const update = new Update(
+      { provider: updateCfg.provider as any, url: updateCfg.url },
+      updateCfg.dirname
+    );
+    const session = new Session();
 
-  tary.on();
-  update.on();
-  session.on();
+    tary.on();
+    update.on();
+    session.on();
 
-  windowInstance.create(customize, opt);
-  tary.create(logo);
-});
+    windowInstance.create(customize, opt);
+    tary.create(logo);
+  })
+  .catch(logError);
