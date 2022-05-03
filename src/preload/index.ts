6d1fe313c8ceaaf094e3e86fb34796a3cbd3c552
youliso <youliso@hotmail.com>
2022-05-03 23:00:16 +0800

    feat: update

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,6 +1,3 @@
 import { preloadDefaultInit } from 'ym-electron/preload';
-import { isSecondInstanceWin } from '@/cfg/app.json';
 
-preloadDefaultInit({
-  isSecondInstanceWin
-});
+preloadDefaultInit();
