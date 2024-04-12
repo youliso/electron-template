f41b79b3d370e2952a79cf75c46d06c87800e897
youliso <shishax@outlook.com>
2024-04-12 18:08:56 +0800

    add napi-rs load

--- scripts/bytecode/index.js
+++ scripts/bytecode/index.js
@@ -1,12 +1,34 @@
 const compile = require('./compile');
 const external = require('./external');
-const { readFileSync, writeFileSync } = require('fs');
+const { spawn } = require('child_process');
+const { readFileSync, writeFileSync, unlinkSync, renameSync } = require('fs');
+const path = require('path');
+
+const loadBuild = () => {
+  return new Promise((resolve) => {
+    const loadProcess = spawn('npx', ['napi', 'build', '--release'], {
+      shell: true,
+      cwd: path.resolve('scripts/bytecode/load')
+    });
+    loadProcess.stdout.on('data', (data) => {
+      console.log(`  \x1B[34m•\x1B[0m byteCode Load ${data.toString('utf8').trim()}`);
+    });
+    loadProcess.stderr.on('data', (data) => {
+      console.log(`  \x1B[34m•\x1B[0m byteCode Load ${data.toString('utf8').trim()}`);
+    });
+    loadProcess.on('close', (code) => {
+      resolve(code);
+    });
+  });
+};
 
 module.exports = async () => {
   let code = readFileSync('dist/main/index.js', 'utf8');
   const codeData = await compile(external(code));
   writeFileSync('dist/main/index.bin', codeData);
-  writeFileSync('dist/main/index.js', `'use strict';require('./load.js');require('./index.bin');`);
-  writeFileSync('dist/main/load.js', readFileSync('scripts/bytecode/load.js', 'utf8'));
+  await loadBuild();
+  unlinkSync('dist/main/index.bin');
+  renameSync('scripts/bytecode/load/index.node', 'dist/main/index.node');
+  writeFileSync('dist/main/index.js', `require("./index.node").load(module, require);`);
   console.log(`  \x1B[34m•\x1B[0m byteCode applying  \x1B[34m\x1B[0m`);
 };
