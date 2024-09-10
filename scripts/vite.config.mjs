5c33019b2f2ed689db505f8b99e667c3a5cbcce4
youliso <youliso@hotmail.com>
2024-09-10 15:22:48 +0800

    fix

--- scripts/vite.config.mjs
+++ scripts/vite.config.mjs
@@ -4,17 +4,37 @@ const root = resolve('src/renderer');
 const outDir = resolve('dist');
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   root,
   base: './',
-  build: {
-    emptyOutDir: false,
-    outDir
-  },
   resolve: {
-    alias: {
-      '@': resolve('src')
+    alias: { '@': resolve('src') }
+  },
+  esbuild: {
+    legalComments: 'none',
+    drop: mode === 'development' ? [] : ['debugger', 'console']
+  },
+  build: {
+    outDir,
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          let chunkAlias = void 0;
+          if (id.includes('node_modules')) {
+            const name = id.split('node_modules/')[1].split('/');
+            chunkAlias = name[0] === '.pnpm' ? name[1] : name[0];
+          }
+          return chunkAlias;
+        },
+        chunkFileNames: 'static/script/[name]-[hash].js',
+        entryFileNames: 'static/script/[name]-[hash].js',
+        assetFileNames: 'static/asset/[name]-[hash].[ext]'
+      }
     }
   },
+  define: {
+    ...require('./.env.json')
+  },
   plugins: []
-});
+}));
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,6 +1,8 @@
 import { windowLoad, windowShow } from '@youliso/electronic/ipc/window';
 
 windowLoad(() => {
+  console.log('customize', window.customize);
+  console.log('env', process.env);
   document.getElementById('root')!.innerHTML = 'hello word';
   windowShow();
 });
