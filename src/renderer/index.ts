25f86bc7d54c49cddf54e6d6bae164b1afecc55b
youliso <youliso@hotmail.com>
2024-09-11 10:54:17 +0800

    fix

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,4 +1,4 @@
-import { windowLoad, windowShow, windowSingleInstanceOn } from '@youliso/electronic/ipc/window';
+import { windowLoad, windowShow, windowSingleInstanceOn } from '@youliso/electronic/ipc';
 
 windowLoad(() => {
   console.log('customize', window.customize);
@@ -6,6 +6,17 @@ windowLoad(() => {
   windowSingleInstanceOn((argv) => {
     console.log(argv);
   });
-  document.getElementById('root')!.innerHTML = 'hello word';
+  const rootDom = document.getElementById('root');
+  const customizeDom = document.createElement('div');
+  const envDom = document.createElement('div');
+  customizeDom.style.wordBreak = 'break-word';
+  customizeDom.innerText = JSON.stringify(window.customize);
+  envDom.style.wordBreak = 'break-word';
+  envDom.innerText = JSON.stringify(process.env);
+  rootDom?.appendChild(document.createTextNode('env'));
+  rootDom?.appendChild(envDom);
+  rootDom?.appendChild(document.createTextNode('customize'));
+  rootDom?.appendChild(customizeDom);
+  rootDom?.appendChild(document.createTextNode('hello word'));
   windowShow();
 });
