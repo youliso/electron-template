18fb3cee0e09627c139871ef372543fecd43171a
youli <youliso@hotmail.com>
2025-03-13 09:57:48 +0800

    refactor(removeLocales): 优化代码结构并增加可配置性
    - 为模块添加第二个参数 locales，默认值为 ['en', 'zh']
    - 使用箭头函数替换普通的 function 回调
    - 优化文件过滤逻辑，使用 filter 方法提高可读性

--- scripts/plugins/removeLocales.js
+++ scripts/plugins/removeLocales.js
@@ -1,14 +1,14 @@
-const fs = require("node:fs");
+const fs = require('node:fs');
 const { join } = require('node:path');
 
-module.exports = (appOutDir) => {
-    const localeDir = join(appOutDir, 'locales', '/');
-    fs.readdir(localeDir, function (err, files) {
-        if (!(files && files.length)) return;
-        for (let i = 0, len = files.length; i < len; i++) {
-            if (!(files[i].startsWith("en") || files[i].startsWith("zh"))) {
-                fs.unlinkSync(localeDir + files[i]);
-            }
-        }
-    });
+module.exports = (appOutDir, locales = ['en', 'zh']) => {
+  const localeDir = join(appOutDir, 'locales', '/');
+  fs.readdir(localeDir, (_, files) => {
+    if (!(files && files.length)) return;
+    for (let i = 0, len = files.length; i < len; i++) {
+      if (locales.filter((v) => files[i].startsWith(v)).length === 0) {
+        fs.unlinkSync(localeDir + files[i]);
+      }
+    }
+  });
 };
